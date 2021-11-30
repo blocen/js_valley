f30965100e746d9fab15b83f5942592a9198be31
Raphael <89421168+blocen@users.noreply.github.com>
2021-11-30 12:44:34 +0100

    async static html

--- hello.js
+++ hello.js
@@ -2,12 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const { readFile } = require('fs').promises;
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/about', (req, res) => {
-  res.send('Hello About!')
+app.get('/about', async (req, res) => {
+  // res.send('Hello About!')
+  res.send(await readFile('./about.html', 'utf8'));
 })
 
 app.listen(port, () => {
